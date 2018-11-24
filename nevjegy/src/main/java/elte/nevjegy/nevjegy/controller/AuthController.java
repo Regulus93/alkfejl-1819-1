16674b9fa554425e583329bdb52ac0c3fe3165f1
Daniel Bicsak <bicsak.daniel@gmail.com>
2018-11-24 16:08:30 +0100

    Added CrossOrigin annotations onto controllers

--- nevjegy/src/main/java/elte/nevjegy/nevjegy/controller/AuthController.java
+++ nevjegy/src/main/java/elte/nevjegy/nevjegy/controller/AuthController.java
@@ -6,13 +6,11 @@ import elte.nevjegy.nevjegy.security.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
--- nevjegy/src/main/java/elte/nevjegy/nevjegy/controller/BusinessCardCollectorController.java
+++ nevjegy/src/main/java/elte/nevjegy/nevjegy/controller/BusinessCardCollectorController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.security.InvalidParameterException;
 import java.security.Principal;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/BCC")
 public class BusinessCardCollectorController {
